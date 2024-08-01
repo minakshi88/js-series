1fee16bb0e197613a30b9d5e9d686a5df422869f
Minakshi Singh <122713741+minakshi88@users.noreply.github.com>
2024-08-01 08:25:59 +0530

    Add files via upload

--- datatypes.js
+++ datatypes.js
@@ -1,3 +1,23 @@
 "use strict"; //treat all JS code as newer version
 
 // alert(5+6); // we are using node JS not browser
+console.log("Minakshi");
+let name="Mini"
+let age=18
+let isLogged=true;
+
+// Several types of Datatype
+//number=> 2 to power 53
+//bigInt
+//String=>""
+//boolean => true/ false
+//null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+//object 
+console.log(typeof "Aparna");
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
